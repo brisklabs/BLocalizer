a03154d48100bb00e0fc9e3d652455fb1ff50874
Roger Molas <r.molas06@gmail.com>
2019-05-21 14:36:54 +0800

    Add application menus

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
@@ -10,6 +10,12 @@ let mainWindow;
 
 app.on('ready', function () {
     // creat the main window
+    createMainWindow();
+    // create menus
+    createMenu();
+});
+
+function createMainWindow() {
     mainWindow = new BrowserWindow({});
     // Load main HTML file
     mainWindow.loadURL(
@@ -19,4 +25,108 @@ app.on('ready', function () {
             slashes: true
         })
     )
-});
+}
+
+function createMenu() {
+    let isMac = process.platform === 'darwin'
+    const template = [
+        ...(process.platform === 'darwin' ? [{
+            label: app.getName(),
+            submenu: [
+              { role: 'about' },
+              { type: 'separator' },
+              { role: 'services' },
+              { type: 'separator' },
+              { role: 'hide' },
+              { role: 'hideothers' },
+              { role: 'unhide' },
+              { type: 'separator' },
+              { role: 'quit' }
+            ]
+          }] : []),
+        {
+            label: 'File',
+            submenu: [
+                {label: "New File"},
+                {label: "New Window"},
+                {type: 'separator' },
+                {label: "Open"},
+                {label: "Open Recent"},
+                {type: 'separator' },
+                {label: "Save"},
+                {label: "Save as..."},
+                { type: 'separator' },
+                 isMac ? { role: 'close' } : { role: 'quit' }
+            ]
+        },
+        {
+            label: 'Edit',
+            submenu: [
+              { role: 'undo' },
+              { role: 'redo' },
+              { type: 'separator' },
+              { role: 'cut' },
+              { role: 'copy' },
+              { role: 'paste' },
+              ...(isMac ? [
+                { role: 'pasteAndMatchStyle' },
+                { role: 'delete' },
+                { role: 'selectAll' },
+                { type: 'separator' },
+                {
+                  label: 'Speech',
+                  submenu: [
+                    { role: 'startspeaking' },
+                    { role: 'stopspeaking' }
+                  ]
+                }
+              ] : [
+                { role: 'delete' },
+                { type: 'separator' },
+                { role: 'selectAll' }
+              ])
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+              { role: 'reload' },
+              { role: 'forcereload' },
+              { role: 'toggledevtools' },
+              { type: 'separator' },
+              { role: 'resetzoom' },
+              { role: 'zoomin' },
+              { role: 'zoomout' },
+              { type: 'separator' },
+              { role: 'togglefullscreen' }
+            ]
+        },
+        {
+            label: 'Window',
+            submenu: [
+              { role: 'minimize' },
+              { role: 'zoom' },
+              ...(isMac ? [
+                { type: 'separator' },
+                { role: 'front' },
+                { type: 'separator' },
+                { role: 'window' }
+              ] : [
+                { role: 'close' }
+              ])
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+              {
+                label: 'Learn More',
+                click () { require('electron').shell.openExternalSync('https://electronjs.org') }
+              }
+            ]
+          }
+    ];
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+}
